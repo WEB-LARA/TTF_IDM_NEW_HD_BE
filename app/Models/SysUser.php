a977f85354d4bbb5c8a540eb4b5014df25c7056d
SiomayBoy <danieladrianss50@gmail.com>
2022-12-23 20:38:04 +0700

    TEST API 11

--- app/Models/SysUser.php
+++ app/Models/SysUser.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Support\Facades\DB;
 
 class SysUser extends Authenticatable implements JWTSubject
 {

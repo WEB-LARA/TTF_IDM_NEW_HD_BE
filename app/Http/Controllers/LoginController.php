0e287eb8c7906a9108082759e6e6027140595fdb
SiomayBoy <danieladrianss50@gmail.com>
2022-12-23 18:43:27 +0700

    Test APi 10

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -108,15 +108,15 @@ class LoginController extends Controller
                         'PASSWORD' => Hash::make($request->password),
                         'ACTIVE_FLAG' => $request->active_flag
                     ]);
-                    foreach($request->list_supplier as $a){
-                        $sys_map_customer = SysMapSupplier::create([
-                            'USER_ID' => $user->id(),
-                            'SUPP_SITE_CODE' =>$a->supp_site_code,
-                            'BRANCH_CODE' =>  date('Y-m-d'),
-                            'STATUS' => 'Y',
-                            'TRANSFER_FLAG' => 'Y'
-                        ]);
-                    }
+                    // foreach($request->list_supplier as $a){
+                    //     $sys_map_customer = SysMapSupplier::create([
+                    //         'USER_ID' => $user->id(),
+                    //         'SUPP_SITE_CODE' =>$a->supp_site_code,
+                    //         'BRANCH_CODE' =>  date('Y-m-d'),
+                    //         'STATUS' => 'Y',
+                    //         'TRANSFER_FLAG' => 'Y'
+                    //     ]);
+                    // }
     
                 },5);
             }catch (\Exception $e) {

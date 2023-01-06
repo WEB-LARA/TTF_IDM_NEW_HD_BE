96defb1a01cef6fe8059502771c78105fe92ac67
SiomayBoy <danieladrianss50@gmail.com>
2023-01-06 19:34:50 +0700

    Update Insert TTF

--- app/Models/TtfTmpTable.php
+++ app/Models/TtfTmpTable.php
@@ -17,31 +17,32 @@ class TtfTmpTable extends Model
     public function saveToTmpTable($fp_type,$no_fp,$supp_site_id,$branch_code,$fp_date,$dpp_fp,$tax_fp,$data_bpb,$scan_flag){
         $session_id = session()->getId();
         print_r($session_id);
-            // try{
-            //     DB::transaction(function () use ($request,$user){
-            //         $tmpTable = TtfTmpTable::create([
-            //             'USERNAME' => $request->username,
-            //             'USER_EMAIL' => $request->email,
-            //             'RESET_FLAG' => $request->reset_flag,
-            //             'PASSWORD' => Hash::make($request->password),
-            //             'ACTIVE_FLAG' => $request->active_flag,
-            //             'CREATION_DATE' => date('Y-m-d')
-            //         ]);
-            //         foreach($request->list_supplier as $a){
-            //             // print_r();
-            //             $sys_map_customer = SysMapSupplier::create([
-            //                 'USER_ID' => $user->ID_USER,
-            //                 'SUPP_SITE_CODE' =>$a['supp_site_code'],
-            //                 'BRANCH_CODE' =>  $a['supp_branch_code'],
-            //                 'STATUS' => 'Y',
-            //                 'TRANSFER_FLAG' => 'Y'
-            //             ]);
-            //         }
+            try{
+                DB::transaction(function () use ($request,$user){
+                    foreach($datga_bpb as $a){
+                        $tmpTable = TtfTmpTable::create([
+                            'SEQ_NUM' => 1,
+                            'FP_TYPE' => $fp_type,
+                            'SUPP_SITE' => '121',
+                            'CABANG' => $branch_code,
+                            'NO_FP' => $no_fp,
+                            'NO_NPWP' => 'teest npwp',
+                            'FP_DATE' => $tanggal_fp,
+                            'FP_DPP' => $dpp_fp,
+                            'FP_TAX' => $tax_fp,
+                            'BPB_NUM' => $a['bpb_num'],
+                            'BPB_DATE' => $a['bpb_date'],
+                            'BPB_AMOUNT' => $a['bpb_amount'],
+                            'BPB_PPN' => $a['bpb_ppn'],
+                            'SESS_ID' => $session_id,
+                            'SCAN_FLAG' => $scan_flag
+                        ]);
+                    }
     
-            //     },5);
-            // }catch (\Exception $e) {
+                },5);
+            }catch (\Exception $e) {
 
-            //     return $e->getMessage();
-            // }
+                return $e->getMessage();
+            }
     }
 }

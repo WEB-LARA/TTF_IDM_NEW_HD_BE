e6fe915b031c3929f480c818c932dd3ac6b05559
SiomayBoy <danieladrianss50@gmail.com>
2023-01-09 23:09:05 +0700

    UPDATE API TTF INSERT

--- app/Http/Controllers/InputTTfController.php
+++ app/Http/Controllers/InputTTfController.php
@@ -73,7 +73,7 @@ class InputTTfController extends Controller
         $data = $ttf_tmp_table->getDataTTfTmpForInsertTTf($request->supp_site_code);
         
         // print_r($data);
-            DB::transaction(function () use($data){
+            DB::transaction(function () use($data,$request){
                 foreach($data as $a){
                     // print_r($a['SUPP_SITE']);
                     $ttf_type = $a['TTF_TYPE'];

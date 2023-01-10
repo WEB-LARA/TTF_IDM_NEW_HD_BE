e54fe79ddba0fd8ce042d59de333466d52e80221
SiomayBoy <danieladrianss50@gmail.com>
2023-01-10 15:09:09 +0700

    Update API INSERT

--- app/Models/TtfTmpTable.php
+++ app/Models/TtfTmpTable.php
@@ -87,8 +87,7 @@ class TtfTmpTable extends Model
                                           AND tdb.VENDOR_SITE_CODE = ?
                                           AND tdb.VENDOR_SITE_ID IS NOT NULL
                                           AND tdb.VENDOR_SITE_ID <> 0
-                                  ) as BPB_ID")
-                    ->setBindings([$branch_code, $supp_site_code])
+                                  ) as BPB_ID",[$branch_code, $supp_site_code])
                     ->get();
 
         return $getData;

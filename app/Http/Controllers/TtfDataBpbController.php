8cb718d65204fe821326422ae9c535f553488aa6
SiomayBoy <danieladrianss50@gmail.com>
2023-01-11 20:35:34 +0700

    Update API PREPOPULATED

--- app/Http/Controllers/TtfDataBpbController.php
+++ app/Http/Controllers/TtfDataBpbController.php
@@ -9,11 +9,11 @@ class TtfDataBpbController extends Controller
 {
     public function getDataBPBPerSupplier(Request $request){
         $ttf_data_bpb = new TtfDataBpb();
-        $getData = $ttf_data_bpb->getDataBPBPerSupplier($request->supp_site_code,$request->branch_code,$request->bpb_id);
-
-        return response()->json([
-                'status' => 'success',
-                'data' => $getAllData,
-            ]);
+        // $getData = $ttf_data_bpb->getDataBPBPerSupplier($request->supp_site_code,$request->branch_code,$request->data_bpb);
+        print_r($request->data_bpb);
+        // return response()->json([
+        //         'status' => 'success',
+        //         'data' => $getAllData,
+        //     ]);
     }
 }

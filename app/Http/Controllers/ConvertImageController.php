992188d5bc80abee74a31d182e52ee8e69dc8eb1
SiomayBoy <danieladrianss50@gmail.com>
2023-01-11 18:23:23 +0700

    Update API INSERT

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -10,10 +10,15 @@ class ConvertImageController extends Controller
     public function index()
     {
         // phpinfo();
-        print_r(public_path());
-        $imgExt = new Imagick();
-        $imgExt->readImage('/usr/src/app/010.002-22.09707040.pdf');
-        $imgExt->writeImages('pdf_image_doc.png', true);
-        dd("Document has been converted");
+        // print_r(public_path());
+        $fileone = realpath('010.002-22.09707040.pdf');
+            
+        if (!is_readable($fileone)) {
+            echo 'file not readable';
+        }
+        // $imgExt = new Imagick();
+        // $imgExt->readImage('/usr/src/app/010.002-22.09707040.pdf');
+        // $imgExt->writeImages('pdf_image_doc.png', true);
+        // dd("Document has been converted");
     }
 }

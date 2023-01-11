cedff4676dc693373fde58399a665891ce34bf72
SiomayBoy <danieladrianss50@gmail.com>
2023-01-11 16:10:55 +0700

    Update API INSERT

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -9,11 +9,11 @@ class ConvertImageController extends Controller
 {
     public function index()
     {
-        phpinfo();
+        // phpinfo();
         // print_r(public_path());
-        // $imgExt = new Imagick();
-        // $imgExt->readImage('/opt/lampp/htdocs/TTF_IDM_NEW_HD_BE/storage/pdf/010.002-22.09707040.pdf');
-        // $imgExt->writeImages('pdf_image_doc.png', true);
-        // dd("Document has been converted");
+        $imgExt = new Imagick();
+        $imgExt->readImage('/opt/lampp/htdocs/TTF_IDM_NEW_HD_BE/storage/pdf/010.002-22.09707040.pdf');
+        $imgExt->writeImages('pdf_image_doc.png', true);
+        dd("Document has been converted");
     }
 }

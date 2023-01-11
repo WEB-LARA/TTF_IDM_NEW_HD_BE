c6abd59f5712322f6cfaa094c4f2fd95ab82dcad
SiomayBoy <danieladrianss50@gmail.com>
2023-01-11 20:40:34 +0700

    Update API PREPOPULATED

--- app/Http/Controllers/TtfDataBpbController.php
+++ app/Http/Controllers/TtfDataBpbController.php
@@ -9,8 +9,8 @@ class TtfDataBpbController extends Controller
 {
     public function getDataBPBPerSupplier(Request $request){
         $ttf_data_bpb = new TtfDataBpb();
+        $id_bpb = array();
         if($request->data_bpb){
-            $id_bpb = array();
             foreach($request->data_bpb as $a){
                 array_push($id_bpb,$a['bpb_id']);
             }

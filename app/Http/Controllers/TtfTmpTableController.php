cad7a5e6a98d7398e81743d5100efa02d200102c
Daniel Adrian <Daniel.Adrian@indomaret.group>
2023-01-12 15:35:18 +0700

    UPDATE TMP TTF

--- app/Http/Controllers/TtfTmpTableController.php
+++ app/Http/Controllers/TtfTmpTableController.php
@@ -7,9 +7,9 @@ use App\Models\TtfTmpTable;
 
 class TtfTmpTableController extends Controller
 {
-    public function getDataTmpTtfBySuppCodeAndBranch(Request $request){
+    public function getDataTmpTtfBySessId(Request $request){
         $ttf_tmp_table = new TtfTmpTable();
-        $data = $ttf_tmp_table->getDataTmpTtfBySuppCodeAndBranch($request->supp_site_code,$request->branch_code);
+        $data = $ttf_tmp_table->getDataTmpTtfBySessId($request->supp_site_code,$request->branch_code);
         return response()->json([
                 'status' => 'success',
                 'data' => $data,

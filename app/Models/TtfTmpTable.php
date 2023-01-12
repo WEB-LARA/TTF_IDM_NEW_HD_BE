9b5c72c611f95c43d5c7049fda1de61bbf0831a4
Daniel Adrian <Daniel.Adrian@indomaret.group>
2023-01-12 16:59:38 +0700

    UPDATE API INSERT

--- app/Models/TtfTmpTable.php
+++ app/Models/TtfTmpTable.php
@@ -75,7 +75,7 @@ class TtfTmpTable extends Model
     }
 
     public function getDataTTFTmpBPB($supp_site_code,$branch_code,$no_fp){
-        $getData = TtfTmpTable::where('SUPP_SITE',$supp_site_code)
+        $getData = TtfTmpTable::where('SUPP_SITEE',$supp_site_code)
                     ->where('CABANG',$branch_code)
                     ->where('NO_FP',$no_fp)
                     ->select('BPB_NUM','BPB_DATE','BPB_AMOUNT')

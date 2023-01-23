a5d8ae955b264c2a2151a2c2469ae5b1b1695551
SiomayBoy <danieladrianss50@gmail.com>
2023-01-23 15:16:37 +0700

    Tes Barcoded

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -24,7 +24,7 @@ class ConvertImageController extends Controller
         // }
         $imgExt = new Imagick();
         $imgExt->readImage(public_path('/file_djp_ttf_idm/1674193948.pdf'));
-        $imgExt->writeImages(public_path('/file_djp_ttf_idm/Tes gambar.png'), true);
+        $imgExt->writeImages(public_path('/file_djp_ttf_idm/Tesgambarbarcode.png'), true);
         dd("Document has been converted");
     }
     public function fileUploadPost(Request $request)

18c43ffe1c9f62d34c082e152f150b34521a8de8
SiomayBoy <danieladrianss50@gmail.com>
2023-01-23 17:14:01 +0700

    Tes Barcoded

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -49,7 +49,7 @@ class ConvertImageController extends Controller
     }
     public function readQr(){
         // phpinfo();
-        $qrcode = new QrReader(public_path('/file_djp_ttf_idm/tesbarcodekakemma2.jpeg'));
+        $qrcode = new QrReader(public_path('/file_djp_ttf_idm/Tesgambarbarcode3sendiri.jpeg'));
         // print_r($qrcode);
         $text = $qrcode->text();
         // print_r("TES");

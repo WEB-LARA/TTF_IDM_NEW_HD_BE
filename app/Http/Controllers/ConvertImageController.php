0be032bc60fb4f708856fc1ad36f627646eb8bde
Daniel Adrian <Daniel.Adrian@indomaret.group>
2023-01-24 10:54:27 +0700

    Test COnvert

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -26,10 +26,12 @@ class ConvertImageController extends Controller
         $imgExt = new Imagick();
         // $imgExt->setResolution(120,120);
         $imgExt->readImage(public_path('/file_djp_ttf_idm/1674531730.pdf'));
+        $imgExt->setImageFormat('png');
         $imgExt->setBackgroundColor(new ImagickPixel('white'));
         $imgExt->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE );
         $imgExt->setImageColorSpace(Imagick::COLORSPACE_SRGB);
         $imgExt->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
+        $imgExt->setImageCompressionQuality(90);
         // $imgExt->setImageOpacity(0);
         // $imgExt->setResolution( 300, 300 );
         // $imgExt->setImageAlphaChannel(9);

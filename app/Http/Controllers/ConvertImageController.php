5637292a3916b487a83efaf7351a93f1c3ec9a33
SiomayBoy <danieladrianss50@gmail.com>
2023-01-25 20:33:32 +0700

    Test123

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -117,7 +117,7 @@ class ConvertImageController extends Controller
     public function readQr(){
         // phpinfo();
         ini_set('memory_limit', '-1');
-        $qrcode = new QrReader(public_path('/file_djp_ttf_idm/1674653470.png'));
+        $qrcode = new QrReader(public_path('/file_djp_ttf_idm/1674653566.png'));
         // print_r($qrcode);
         $text = $qrcode->text();
         // print_r("TES");

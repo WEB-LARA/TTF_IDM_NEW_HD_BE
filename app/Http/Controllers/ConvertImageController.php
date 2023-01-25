2ce490c637ddd8e085c20c39a4c1c3f7c68cb511
SiomayBoy <danieladrianss50@gmail.com>
2023-01-25 20:39:05 +0700

    Test123

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -31,36 +31,23 @@ class ConvertImageController extends Controller
    
         if($request->file->move(public_path('/file_djp_ttf_idm'), $fileName)){
             // Convert Fp ke Gambar
-            // print_r("MASOK");
             $fileNameConverted = $this->convertFpPdfToImage($fileName);
             $linkQr = '';
             foreach ($fileNameConverted as $a){
                 print_r($a);
                 $linkQr .= $this->readQr($a);
             }
-            // $expLodeFileName = explode(".",$fileNameConverted);
-            // print_r($expLodeFileName);
-            // Scan Qr Faktur Pajak
-            // $linkQr = $this->readQr($fileNameConverted);
-
             $explodeLink = explode("/",$linkQr);
             print_r($explodeLink);
         }else{
             print_r("GAGAl");
         }
-        // print_r($fileName);
-   
-        // return back()
-        //     ->with('success','You have successfully upload file.')
-        //     ->with('file',$fileName);
    
     }
     
     public function convertFpPdfToImage($filename){
         $getNumberPages = new Imagick(public_path('/file_djp_ttf_idm/'.$filename));
         $numOfPages = $getNumberPages->getNumberImages();
-        // print_r($numOfPages);
-        // print_r($filename);
         $imgExt = new Imagick();
         if($numOfPages > 1){
             $imgExt->setResolution(150,150);
@@ -79,12 +66,6 @@ class ConvertImageController extends Controller
             $imgExt->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
             $imgExt->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
             $imgExt->setOption('png:bit-depth', '16');
-            // $imgExt->readImage(public_path('/file_djp_ttf_idm/'.$filename.'[0]'));
-            // $imgExt->setImageBackgroundColor('white');
-            // $imgExt->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
-            // $imgExt->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
-            // $imgExt->setOption('png:bit-depth', '16');
-            // $imgExt->writeImages(public_path('/file_djp_ttf_idm/'.$fileNameConverted), true);
             if($numOfPages>1){
                 array_push($arrayFileConverted,$namaFile.'-'.$counter.'.'.$format);
             }else{
@@ -95,33 +76,11 @@ class ConvertImageController extends Controller
         $imgExt->writeImages(public_path('/file_djp_ttf_idm/'.$fileNameConverted), false);
 
         return $arrayFileConverted;
-        // $imgExt = new Imagick();
-        // $imgExt->readImage(public_path('/file_djp_ttf_idm/'.$filename));
-        // $fileNameConverted = time().'.'.'png';
-        // $i = 0;
-        // foreach($imgExt as $i=>$imgExt){
-        //     $imgExt->readImage(public_path('/file_djp_ttf_idm/'.$filename.'['.$i.']'));
-        //     $imgExt->setImageBackgroundColor('white');
-        //     $imgExt->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
-        //     $imgExt->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
-        //     $imgExt->setOption('png:bit-depth', '16');
-        //     print_r("1");
-        //     echo "<br>";
-        //     $imgExt->writeImages(public_path('/file_djp_ttf_idm/'.$fileNameConverted.'['.$i.']'), true);
-        //     $i ++;
-        // }
-
-        // return $fileNameConverted;
     }
     public function readQr($filename){
-        // phpinfo();
         ini_set('memory_limit', '-1');
         $qrcode = new QrReader(public_path('/file_djp_ttf_idm/'.$filename));
-        // print_r($qrcode);
         $text = $qrcode->text();
-        // print_r("TES");
-        // print_r($text);
-        // print_r ($text);
         return $text;
     }
 }

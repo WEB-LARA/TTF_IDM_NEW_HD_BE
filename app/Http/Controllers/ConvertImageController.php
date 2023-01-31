efbce611ff3917589b2ab73f9f4edfe3a12fa113
Daniel Adrian <Daniel.Adrian@indomaret.group>
2023-01-31 11:56:12 +0700

    Update api validasi pdf

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -23,6 +23,7 @@ class ConvertImageController extends Controller
     }
     public function fileUploadPost(Request $request)
     {
+        header('Access-Control-Allow-Origin: *');
         $request->validate([
             'file' => 'required|mimes:pdf|max:2048',
         ]);
@@ -76,7 +77,6 @@ class ConvertImageController extends Controller
     }
     
     public function convertFpPdfToImage($filename){
-        header('Access-Control-Allow-Origin: *');
         $getNumberPages = new Imagick(public_path('/file_djp_ttf_idm/'.$filename));
         $numOfPages = $getNumberPages->getNumberImages();
         $imgExt = new Imagick();

0c6c0d418ab21ebfe32035cf4ae4feaeb72cf5f7
SiomayBoy <danieladrianss50@gmail.com>
2023-02-06 00:09:56 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/SysAnnouncementController.php
+++ app/Http/Controllers/SysAnnouncementController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SysAnnouncement;
+use Illuminate\Support\Facades\DB;
 
 class SysAnnouncementController extends Controller
 {

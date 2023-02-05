f463e1d8eb67added6fb699e440f71596a54c5fd
SiomayBoy <danieladrianss50@gmail.com>
2023-02-06 00:17:03 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/SysAnnouncementController.php
+++ app/Http/Controllers/SysAnnouncementController.php
@@ -8,10 +8,6 @@ use Illuminate\Support\Facades\DB;
 
 class SysAnnouncementController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:api');
-    }
     public function createAnnouncement(Request $request){
         $announcement = new SysAnnouncement();
         try{

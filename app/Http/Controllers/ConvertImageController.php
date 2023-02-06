50c585149c9b1fb98b1f6ac7f528b1cf7a4597e0
SiomayBoy <danieladrianss50@gmail.com>
2023-02-06 19:16:59 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -127,7 +127,7 @@ class ConvertImageController extends Controller
         $dir = public_path('/file_temp_fp/'.$year);
         if ( !file_exists( $dir ) && !is_dir( $dir ) ) {
             mkdir( $dir );     
-            chmod($dir, 777);
+            chmod($dir, 0777);
             print_r("TIDAK ADA");  
         }else{
             print_r("ADA");

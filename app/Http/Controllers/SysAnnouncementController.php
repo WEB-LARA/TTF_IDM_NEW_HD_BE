82d80723954ae103d58ab3e49a1ef38b98337fe3
SiomayBoy <danieladrianss50@gmail.com>
2023-02-06 11:17:45 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/SysAnnouncementController.php
+++ app/Http/Controllers/SysAnnouncementController.php
@@ -52,6 +52,58 @@ class SysAnnouncementController extends Controller
         }
     }
 
+    public function updateAnnouncement(Request $request){
+        $announcement = new SysAnnouncement();
+        $files = $request->file;
+        // foreach ($files as $key => $file){
+        //     print_r("TEST");
+        // }
+
+        if($request->file_pengumuman){
+            $fileName = time().'.'.$request->file_pengumuman->extension();
+        }
+   
+        try{
+            DB::transaction(function () use ($request,$fileName){
+                if($request->file_pengumuman->move(public_path('/file_pengumuman'), $fileName)){
+                    // Convert Fp ke Gambar
+                    if($request->file_pengumuman){
+                        $announcement = SysAnnouncement::where('ID_PENGUMUMAN',$request->id_pengumuman)->update([
+                            'JUDUL_PENGUMUMAN' => $request->judul_pengumuman,
+                            'ISI_PENGUMUMAN' => $request->isi_pengumuman,
+                            'START_DATE' => $request->start_date,
+                            'END_DATE' => $request->end_date,
+                            'FILENAME' => $fileName
+                        ]);
+
+                    }else{
+                        $announcement = SysAnnouncement::where('ID_PENGUMUMAN',$request->id_pengumuman)->update([
+                            'JUDUL_PENGUMUMAN' => $request->judul_pengumuman,
+                            'ISI_PENGUMUMAN' => $request->isi_pengumuman,
+                            'START_DATE' => $request->start_date,
+                            'END_DATE' => $request->end_date
+                        ]);
+                    }
+                }
+
+            },5);
+        }catch (\Exception $e) {
+            return $e->getMessage();
+        }
+
+        if($announcement){
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Announcement Berhasil dibuat!'
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 'gagal',
+                'message' => 'Announcement Gagal dibuat!'
+            ],400);
+        }
+    }
+
     public function getDataAnnouncement(){
         $announcement = new SysAnnouncement();
 

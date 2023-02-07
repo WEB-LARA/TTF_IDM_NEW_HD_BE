b8ac27e631f5e0f1d4bd7119c4cfee3659c26463
SiomayBoy <danieladrianss50@gmail.com>
2023-02-07 16:18:13 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/InputTTfController.php
+++ app/Http/Controllers/InputTTfController.php
@@ -170,7 +170,6 @@ class InputTTfController extends Controller
                         'SOURCE' => "WEB",
                         'FLAG_GO' => $request->flag_go,
                         'FLAG_PPN' => $request->flag_ppn,
-                        'JUMLAH_FP' => $a['JUMLAH_FP'],
                         'SUM_DPP_FP' => $a['SUM_DPP_FP'],
                         'SUM_TAX_FP' => $a['SUM_TAX_FP'],
                         'CREATED_BY' => $user_id,

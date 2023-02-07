16c59201653a4a4848c7363285d6ede68706a736
SiomayBoy <danieladrianss50@gmail.com>
2023-02-07 10:54:43 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/ConvertImageController.php
+++ app/Http/Controllers/ConvertImageController.php
@@ -56,11 +56,13 @@ class ConvertImageController extends Controller
                 "." .
                 substr($explodeLink[6], 5, 8);
             if($request->no_npwp == $npwp_penjual && substr($request->no_faktur, 4) == $no_faktur){
+                unlink(public_path('/file_temp_fp/'.$fileName));
                 return response()->json([
                         'status' => 'success',
                         'message' => 'validated',
                     ]);
             }else{
+                unlink(public_path('/file_temp_fp/'.$fileName));
                 return response()->json([
                         'status' => 'success',
                         'message' => 'rejected',

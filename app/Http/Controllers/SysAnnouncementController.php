ef6a3058cd7c910a26f1aca8ae6ef4a832d8ce38
SiomayBoy <danieladrianss50@gmail.com>
2023-02-07 14:57:22 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/SysAnnouncementController.php
+++ app/Http/Controllers/SysAnnouncementController.php
@@ -154,7 +154,7 @@ class SysAnnouncementController extends Controller
         //PDF file is stored under project/public/download/info.pdf
         $announcement = new SysAnnouncement();
         $getFilename = $announcement->getFilename($request->id_pengumuman);
-        print_r($getFilename->FILENAME);
+        print_r($getFilename);
         // $file= public_path(). "/download/info.pdf";
 
         // $headers = array(

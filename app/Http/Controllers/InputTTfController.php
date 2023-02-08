d8c170db4de1527c9569b3989b3db38974b8e809
SiomayBoy <danieladrianss50@gmail.com>
2023-02-08 22:24:06 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/InputTTfController.php
+++ app/Http/Controllers/InputTTfController.php
@@ -291,7 +291,7 @@ class InputTTfController extends Controller
                 print_r($fileName);
                 echo "<br>";
                 // $request->file->move(public_path('/file_temp_fp'), $fileName)
-                if($file->store(public_path('/file_djp_ttf_idm/2022/Feb/005/230052473793'), $fileName)){
+                if($file->move(public_path('/file_djp_ttf_idm/2022/Feb/005/230052473793'), $fileName)){
                     print_r("SUKSES");
                 }else{
                     print_r("GAGAL");

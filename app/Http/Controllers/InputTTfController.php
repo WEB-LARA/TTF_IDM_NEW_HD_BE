e85c2b9d1d8d104b33c72c5c3b1d6702a5f8b634
SiomayBoy <danieladrianss50@gmail.com>
2023-02-08 22:49:15 +0700

    Test

--- app/Http/Controllers/InputTTfController.php
+++ app/Http/Controllers/InputTTfController.php
@@ -302,7 +302,7 @@ class InputTTfController extends Controller
                         "FILENAME" => $fileName,
                         "REAL_NAME" => $real_name,
                         "PATH_FILE" => public_path('/file_djp_ttf_idm/2023/Feb/005/230052473794/'.$fileName),
-                        "CREATED_DATE" => date('Y-m-d')
+                        "CREATION_DATE" => date('Y-m-d')
                     ]);
                 }
             }

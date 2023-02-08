cb94f2f42d25554652c0462492838d4bb565dca5
SiomayBoy <danieladrianss50@gmail.com>
2023-02-08 22:16:21 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/InputTTfController.php
+++ app/Http/Controllers/InputTTfController.php
@@ -287,6 +287,8 @@ class InputTTfController extends Controller
             foreach($request->file('file_lampiran') as $key => $file)
             {
                 $fileName = time().'.'.$file->extension();  
+                print_r($fileName);
+                echo "<br>";
                 // $request->file->move(public_path('/file_temp_fp'), $fileName)
                 $file->move(public_path('/file_djp_ttf_idm/2022/Feb/005/230052473793'), $fileName);
                 print_r("ADA");

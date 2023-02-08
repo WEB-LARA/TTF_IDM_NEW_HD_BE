b32e9d56e506f1f84cab880f030e6c23ecc3460a
SiomayBoy <danieladrianss50@gmail.com>
2023-02-08 13:38:05 +0700

    Test

--- app/Http/Controllers/InputTTfController.php
+++ app/Http/Controllers/InputTTfController.php
@@ -35,7 +35,7 @@ class InputTTfController extends Controller
         $session_id = $request->session_id;
         // $file = $request->file;
         try{
-            DB::transaction(function () use ($fp_type,$no_fp,$supp_site_id,$branch_code,$fp_date,$dpp_fp,$tax_fp,$data_bpb,$scan_flag,$session_id,$file){
+            DB::transaction(function () use ($fp_type,$no_fp,$supp_site_id,$branch_code,$fp_date,$dpp_fp,$tax_fp,$data_bpb,$scan_flag,$session_id){
                 $sys_supp_site = new SysSuppSite();
                 $dataSuppSite = $sys_supp_site->getSiteCodeAndNpwp($supp_site_id,$branch_code);
                 foreach($data_bpb as $a){

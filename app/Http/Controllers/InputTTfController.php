f4cb4640c300397c087f48302d222f8b96f0de7a
SiomayBoy <danieladrianss50@gmail.com>
2023-02-08 20:44:21 +0700

    Test API MASTER PENGUMUMAN

--- app/Http/Controllers/InputTTfController.php
+++ app/Http/Controllers/InputTTfController.php
@@ -254,6 +254,6 @@ class InputTTfController extends Controller
     public function moveFile (){
         $sys_fp_fisik_temp = new SysFpFisikTemp();
         $getDataFpFisik = $sys_fp_fisik_temp->getDataSysFpFisikTmpByNoFp('010.002-21.53047341');
-        print_r($getDataBPBperFP);
+        print_r($getDataFpFisik);
     }
 }

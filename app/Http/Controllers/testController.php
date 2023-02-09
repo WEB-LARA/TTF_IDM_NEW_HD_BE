dfd61b4b7197357bc2c5221695ef4a2b8fb3236e
Vinna Violetta Cornela <Vinna.VC@indomaret.group>
2023-02-09 15:53:54 +0700

    test

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\testModel;
+
 class testController extends Controller
 {
     //
@@ -13,5 +15,15 @@ class testController extends Controller
 
     public function get(){
         print_r("TEST");
+    }
+
+    public function getdata(){
+        $test_model = new testModel();
+        $data = $test_model->getdata();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $data
+        ]);
     }
 }
--- app/Models/testModel.php
+++ app/Models/testModel.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class testModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'ttf_data_bpb';
+
+    protected $primaryKey = 'ID';
+    public $timestamps = false;
+    protected $fillable = [
+        'BPB_ID',
+        'BPB_NUMBER',
+        'BPB_DATE',
+    ];
+
+    public function getdata(){
+        $getdata = testModel::get();
+        return $getdata;
+    }
 }

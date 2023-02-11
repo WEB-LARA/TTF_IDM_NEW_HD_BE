7db73a57eda91862fc16d2665871b35f4418c307
Vinna Violetta Cornela <Vinna.VC@indomaret.group>
2023-02-11 16:05:22 +0700

    test

--- app/Models/testModel.php
+++ app/Models/testModel.php
@@ -32,7 +32,6 @@ class testModel extends Model
         $data = testModel::join('ttf_headers', 'ttf_headers.VENDOR_SITE_CODE', '=', 'ttf_data_bpb.VENDOR_SITE_CODE')
               ->join('ttf_fp', 'ttf_fp.TTF_ID', '=', 'ttf_headers.TTF_ID')
               ->join('sys_supplier', 'sys_supplier.SUPP_ID', '=', 'ttf_fp.TTF_ID')
-              ->select('ttf_data_bpb.VENDOR_SITE_CODE','sys_supplier.SUPP_NAME','ttf_data_bpb.BPB_NUMBER','ttf_data_bpb.BPB_DATE','ttf_data_bpb.BPB_DPP','ttf_data_bpb.BPB_TAX','ttf_fp.FP_NUM','ttf_fp.FP_DATE','ttf_fp.FP_DPP_AMT','ttf_fp.FP_TAX_AMT','ttf_headers.TTF_NUM','ttf_headers.TTF_DATE','ttf_headers.TTF_RETURN_DATE','ttf_headers.TTF_STATUS')
               ->whereRaw(
                 '( 
                     CASE 
@@ -45,6 +44,8 @@ class testModel extends Model
                     END
                 )'
             )
+              ->select('ttf_data_bpb.VENDOR_SITE_CODE',
+              'sys_supplier.SUPP_NAME','ttf_data_bpb.BPB_NUMBER','ttf_data_bpb.BPB_DATE','ttf_data_bpb.BPB_DPP','ttf_data_bpb.BPB_TAX','ttf_fp.FP_NUM','ttf_fp.FP_DATE','ttf_fp.FP_DPP_AMT','ttf_fp.FP_TAX_AMT','ttf_headers.TTF_NUM','ttf_headers.TTF_DATE','ttf_headers.TTF_RETURN_DATE','ttf_headers.TTF_STATUS')
               ->take(10)
               ->get();
 

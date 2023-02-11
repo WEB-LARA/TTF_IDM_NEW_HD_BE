bde3a13a2345ac2b143b5f063d0432db0334303d
Vinna Violetta Cornela <Vinna.VC@indomaret.group>
2023-02-11 15:59:37 +0700

    test

--- app/Models/testModel.php
+++ app/Models/testModel.php
@@ -33,6 +33,18 @@ class testModel extends Model
               ->join('ttf_fp', 'ttf_fp.TTF_ID', '=', 'ttf_headers.TTF_ID')
               ->join('sys_supplier', 'sys_supplier.SUPP_ID', '=', 'ttf_fp.TTF_ID')
               ->select('ttf_data_bpb.VENDOR_SITE_CODE','sys_supplier.SUPP_NAME','ttf_data_bpb.BPB_NUMBER','ttf_data_bpb.BPB_DATE','ttf_data_bpb.BPB_DPP','ttf_data_bpb.BPB_TAX','ttf_fp.FP_NUM','ttf_fp.FP_DATE','ttf_fp.FP_DPP_AMT','ttf_fp.FP_TAX_AMT','ttf_headers.TTF_NUM','ttf_headers.TTF_DATE','ttf_headers.TTF_RETURN_DATE','ttf_headers.TTF_STATUS')
+              ->whereRaw(
+                '( 
+                    CASE 
+                         WHEN ttf_headers.TTF_STATUS = "" THEN "DRAFT"
+                         WHEN ttf_headers.TTF_STATUS = "C" THEN "CANCEL"
+                         WHEN ttf_headers.TTF_STATUS = "E" THEN "EXPIRED"
+                         WHEN ttf_headers.TTF_STATUS = "R" THEN "REJECTED"
+                         WHEN ttf_headers.TTF_STATUS = "S" THEN "SUBMITTED"
+                         ELSE "VALIDATED"
+                    END
+                )'
+            )
               ->take(10)
               ->get();
 

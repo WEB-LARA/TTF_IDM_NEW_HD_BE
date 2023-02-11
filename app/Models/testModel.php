87fb4c9301b9b451f21213fd33e8e0fe5b14c98c
Vinna Violetta Cornela <Vinna.VC@indomaret.group>
2023-02-11 14:56:27 +0700

    test

--- app/Models/testModel.php
+++ app/Models/testModel.php
@@ -29,10 +29,10 @@ class testModel extends Model
     }
 
     public function inquirydata(){
-        $data = testModel::join('ttf_fp', 'ttf_fp.TTF_FP_ID', '=', 'ttf_data_bpb.BPB_ID')
-              ->join('ttf_headers', 'ttf_headers.TTF_ID', '=', 'ttf_fp.TTF_ID')
+        $data = testModel::join('ttf_headers', 'ttf_headers.VENDOR_SITE_CODE', '=', 'ttf_data_bpb.VENDOR_SITE_CODE')
+              ->join('ttf_fp', 'ttf_fp.TTF_ID', '=', 'ttf_headers.TTF_ID')
               ->join('sys_supplier', 'sys_supplier.SUPP_ID', '=', 'ttf_fp.TTF_ID')
-              ->select('ttf_data_bpb.VENDOR_SITE_CODE','ttf_data_bpb.BPB_NUMBER','ttf_data_bpb.BPB_DATE','ttf_data_bpb.BPB_DPP','ttf_data_bpb.BPB_TAX','ttf_fp.FP_NUM','ttf_fp.FP_DATE','ttf_fp.FP_DPP_AMT','ttf_fp.FP_TAX_AMT','ttf_headers.TTF_NUM','ttf_headers.TTF_DATE','ttf_headers.TTF_RETURN_DATE','ttf_headers.TTF_STATUS','sys_supplier.SUPP_NAME')
+              ->select('ttf_data_bpb.VENDOR_SITE_CODE','sys_supplier.SUPP_NAME','ttf_data_bpb.BPB_NUMBER','ttf_data_bpb.BPB_DATE','ttf_data_bpb.BPB_DPP','ttf_data_bpb.BPB_TAX','ttf_fp.FP_NUM','ttf_fp.FP_DATE','ttf_fp.FP_DPP_AMT','ttf_fp.FP_TAX_AMT','ttf_headers.TTF_NUM','ttf_headers.TTF_DATE','ttf_headers.TTF_RETURN_DATE','ttf_headers.TTF_STATUS')
               ->get();
 
         return $data;

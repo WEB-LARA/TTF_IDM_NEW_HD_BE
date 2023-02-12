4a3f6145e47196dd0b08456540d2e421310f329a
SiomayBoy <danieladrianss50@gmail.com>
2023-02-13 01:17:42 +0700

    Test

--- app/Models/TtfUploadTmp.php
+++ app/Models/TtfUploadTmp.php
@@ -51,7 +51,7 @@ class TtfUploadTmp extends Model
         return $data;
     }
     public function validateDoubleDPP($session_id,$dpp,$no_fp){
-        $data = TtfUploadTmp::where('SESS_ID',$session_id)->where('FP_DPP',$dpp)->where('NO_FP',$no_fp)->count();
+        $data = TtfUploadTmp::where('SESS_IDD',$session_id)->where('FP_DPP',$dpp)->where('NO_FP',$no_fp)->count();
         return $data;
     }
 }

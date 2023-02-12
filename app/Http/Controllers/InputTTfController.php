3c024062562f2cb9b41cdf05ca6f7d53f024e0ac
SiomayBoy <danieladrianss50@gmail.com>
2023-02-12 21:52:12 +0700

    Test

--- app/Http/Controllers/InputTTfController.php
+++ app/Http/Controllers/InputTTfController.php
@@ -572,7 +572,7 @@ class InputTTfController extends Controller
                 //     ->num_rows();
                 $ttf_data_bpb = new TtfDataBpb();
 
-                $getDataBpb= $ttf_data_bpb->validateBPBByBPBNumber();
+                $getDataBpb= $ttf_data_bpb->validateBPBByBPBNumber($a->BPB_NUM);
                 print_r($getDataBpb);
                 // if ($row > 0)
                 // {

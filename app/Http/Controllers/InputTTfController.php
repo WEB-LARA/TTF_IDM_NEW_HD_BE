cae0c21f92b330de3a13dc8d4875918b663da574
SiomayBoy <danieladrianss50@gmail.com>
2023-02-12 20:39:25 +0700

    Test

--- app/Http/Controllers/InputTTfController.php
+++ app/Http/Controllers/InputTTfController.php
@@ -394,9 +394,8 @@ class InputTTfController extends Controller
                             if($flag_error == false){
                                 while (!feof($file_handle)) {
                                     $data_csv = fgetcsv($file_handle, 1000, $request->delimiter);
-                                    print_r($data_csv[0]);
                                     if($data_csv != false){
-                                        $fp_type = 0;
+                                        $fp_type = 1;
                                         if ($data_csv[1] == 'STD')
                                         {
                                             $fp_type = 1;

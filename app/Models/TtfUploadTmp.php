bec7ca17466185577e0cc6f9fd16b7691534e034
SiomayBoy <danieladrianss50@gmail.com>
2023-02-13 02:25:08 +0700

    Test

--- app/Models/TtfUploadTmp.php
+++ app/Models/TtfUploadTmp.php
@@ -65,14 +65,6 @@ class TtfUploadTmp extends Model
     public function validateDoubleBPB($session_id,$no_fp,$bpb_num){
         $data = TtfUploadTmp::where('SESS_ID',$session_id)->where('NO_FP','<>',$no_fp)->where('BPB_NUM',$bpb_num)->count();
         return $data;
-                // $statement = 'SELECT * from ttf_upload_tmp where SESS_ID = ? and BPB_NUM = ? and NO_FP <> ?';
-                // $row = $this
-                //     ->db
-                //     ->query($statement, array(
-                //     $sess_id,
-                //     $a->BPB_NUM,
-                //     $a->NO_FP
-                // ));
     }
     public function validateBranchInOneFp($session_id,$no_fp,$branch){
         $data = TtfUploadTmp::where('SESS_ID',$session_id)->where('CABANG','<>',$branch)->where('NO_FP',$no_fp)->count();
@@ -80,6 +72,19 @@ class TtfUploadTmp extends Model
     }
     public function checkSuppInFp($session_id,$no_fp,$supp_site){
         $data = TtfUploadTmp::where('SESS_ID',$session_id)->where('SUPP_SITE','<>',$supp_site)->where('NO_FP',$no_fp)->count();
+        return $data;
+    }
+
+    public function checkSelisihFP($session_id){
+        $data = TtfUploadTmp::join('prepopulated_fp', 'prepopulated_fp.NOMOR_FAKTUR', '=', 'ttf_upload_tmp.NO_FP')
+        ->where('ttf_upload_tmp.FP_DPP','=','prepopulated_fp.JUMLAH_DPP')
+        ->where('ttf_upload_tmp.FP_TAX','=','prepopulated_fp.JUMLAH_PPN')
+        ->where('ttf_upload_tmp.SESS_ID',$session_id)
+        ->groupBy('ttf_upload_tmp.SEQ_NUM','ttf_upload_tmp.NO_FP')
+        ->select('ttf_upload_tmp.NO_FP','ttf_upload_tmp.FP_DPP','ttf_upload_tmp.FP_TAX')
+        ->selectRaw('abs(ttf_upload_tmp.FP_DPP - sum(ttf_upload_tmp.BPB_AMOUNT))  SELISIH_DPP')
+        ->selectRaw('abs(a.FP_TAX - sum(a.BPB_PSPN)) SELISIH_PPN')
+        ->get();
         return $data;
     }
 }

96893c8ceb6419d42b2279fc420931dd821e423c
SiomayBoy <danieladrianss50@gmail.com>
2023-02-13 09:42:54 +0700

    Test

--- app/Http/Controllers/SysAnnouncementController.php
+++ app/Http/Controllers/SysAnnouncementController.php
@@ -162,4 +162,16 @@ class SysAnnouncementController extends Controller
 
         return Response::download($file, $getFilename->FILENAME.'.pdf', $headers);
     }
+    public function downloadTemplateCsv(){
+        $headers = array(
+            "Content-type"        => "text/csv",
+            "Content-Disposition" => "attachment; filename=$fileName",
+            "Pragma"              => "no-cache",
+            "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+            "Expires"             => "0"
+        );
+        $file= public_path('/template_csv_ttf.csv');
+
+        return Response::download($file, $getFilename->FILENAME.'.pdf', $headers);
+    }
 }

f2b50f84d4486ff66cae1223b44fadb8de4dd8d3
SiomayBoy <danieladrianss50@gmail.com>
2023-02-13 14:17:45 +0700

    Test

--- app/Http/Controllers/TtfDataBpbController.php
+++ app/Http/Controllers/TtfDataBpbController.php
@@ -15,7 +15,7 @@ class TtfDataBpbController extends Controller
                 array_push($id_bpb,$a['bpb_id']);
             }
         }
-        $getData = $ttf_data_bpb->getDataBPBPerSupplier($request->supp_site_code,$request->branch_code,$id_bpb,$request->session_id,$request->flag_go,$request->flag_ppn,$request->tipe_faktur);
+        $getData = $ttf_data_bpb->getDataBPBPerSupplier($request->supp_site_code,$request->branch_code,$id_bpb,$request->session_id,$request->flag_go,$request->flag_ppn,$request->tipe_faktur,$request->jenis_faktur);
         return response()->json([
                 'status' => 'success',
                 'data' => $getData,

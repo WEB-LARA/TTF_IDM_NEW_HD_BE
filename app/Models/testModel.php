d4d1cf369226b9e82147e70ac7d84f077ba4e080
Vinna Violetta Cornela <Vinna.VC@indomaret.group>
2023-02-14 12:15:24 +0700

    test

--- app/Models/testModel.php
+++ app/Models/testModel.php
@@ -37,9 +37,16 @@ class testModel extends Model
     public function getDataInquiryTtf(){
         $data = testModel::leftjoin('ttf_headers', 'ttf_headers.VENDOR_SITE_CODE', '=', 'ttf_data_bpb.VENDOR_SITE_CODE')
               ->leftjoin('ttf_fp', 'ttf_fp.TTF_ID', '=', 'ttf_headers.TTF_ID')
-              ->leftjoin('sys_supp_site', 'sys_supp_site.SUPP_SITE_CODE', '=', 'ttf_data_bpb.VENDOR_SITE_CODE')
-              ->select('ttf_data_bpb.VENDOR_SITE_CODE',
-              \DB::raw('(SELECT SUPP_NAME FROM sys_supplier WHERE sys_supplier.SUPP_ID = sys_supp_site.SUPP_ID) AS SUPP_NAME'),'ttf_data_bpb.BPB_NUMBER','ttf_data_bpb.BPB_DATE','ttf_data_bpb.BPB_DPP','ttf_data_bpb.BPB_TAX','ttf_fp.FP_NUM','ttf_fp.FP_DATE','ttf_fp.FP_DPP_AMT','ttf_fp.FP_TAX_AMT','ttf_headers.TTF_NUM','ttf_headers.TTF_DATE','ttf_headers.TTF_RETURN_DATE',\DB::raw(
+            //   ->leftjoin('sys_supp_site', 'sys_supp_site.SUPP_SITE_CODE', '=', 'ttf_data_bpb.VENDOR_SITE_CODE')
+              ->selectraw('ttf_data_bpb.VENDOR_SITE_CODE',
+              \DB::raw('(SELECT SUPP_NAME FROM sys_supplier WHERE sys_supplier.SUPP_ID = (SELECT 
+                    SUPP_ID
+                FROM
+                    sys_supp_site b
+                WHERE
+                b.SUPP_SITE_CODE = ttf_data_bpb.VENDOR_SITE_CODE
+                AND b.SUPP_BRANCH_CODE = ttf_data_bpb.BRANCH_CODE)) AS SUPP_NAME'),'ttf_data_bpb.BPB_NUMBER','ttf_data_bpb.BPB_DATE','ttf_data_bpb.BPB_DPP','ttf_data_bpb.BPB_TAX','ttf_fp.FP_NUM','ttf_fp.FP_DATE','ttf_fp.FP_DPP_AMT','ttf_fp.FP_TAX_AMT','ttf_headers.TTF_NUM','ttf_headers.TTF_DATE','ttf_headers.TTF_RETURN_DATE',
+                \DB::raw(
                 '( 
                     CASE 
                          WHEN ttf_headers.TTF_STATUS = "" THEN "DRAFT"

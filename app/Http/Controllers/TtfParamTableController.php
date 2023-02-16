383ff8767656b6611c802f3abe0a01cf5fe41feb
Daniel Adrian <Daniel.Adrian@indomaret.group>
2023-02-16 13:49:50 +0700

    TEST

--- app/Http/Controllers/TtfParamTableController.php
+++ app/Http/Controllers/TtfParamTableController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\TtfParamTable;
 class TtfParamTableController extends Controller
 {
-    //
+    public function getMaxBpbAndPpn(){
+        $ttf_param_table = new TtfParamTable();
+
+        $geData = $ttf_param_table->getMaxBpbAndPpn();
+
+        return response()->json([
+                'status' => 'success',
+                'data' => $geData
+            ]);
+    }
 }

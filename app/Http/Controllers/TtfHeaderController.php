872126ca2e4085d129070cf057ad6a91a765db32
SiomayBoy <danieladrianss50@gmail.com>
2023-02-17 02:01:36 +0700

    Test

--- app/Http/Controllers/TtfHeaderController.php
+++ app/Http/Controllers/TtfHeaderController.php
@@ -201,23 +201,23 @@ class TtfHeaderController extends Controller
                 $nomor_ttf = $ttf_header->getTtfNumByTtfId($data);
                 $path_file = $ttf_lampiran->getPathFile($data);
                 // print_r($nomor_ttf);
-                // $getFilediFpFisik = $sys_fp_fisik->getDataByTtfNumber($nomor_ttf[0]->TTF_NUM);
-                // foreach($getFilediFpFisik as $a){
-                //     if(file_exists( $a->PATH_FILE )){
-                //         unlink($a->PATH_FILE);
-                //     }
-                // }
-                // $getFilediLampiran = $ttf_lampiran->getDataTtfLampiranByTTfID($data);
-                // foreach($getFilediLampiran as $a){
-                //     if(file_exists( $a->PATH_FILE )){
-                //         unlink($a->PATH_FILE);
-                //     }
-                // }
-                // $deleteLampiran = $ttf_lampiran->deleteTtfLampiran($data);
-                // $deleteFpFisik = $sys_fp_fisik->deleteSysFpFisik($nomor_ttf[0]->TTF_NUM);
-                // if(file_exists( substr($path_file->PATH_FILE,0,62) )){
-                //     rmdir(substr($path_file->PATH_FILE,0,62));
-                // }
+                $getFilediFpFisik = $sys_fp_fisik->getDataByTtfNumber($nomor_ttf[0]->TTF_NUM);
+                foreach($getFilediFpFisik as $a){
+                    if(file_exists( $a->PATH_FILE )){
+                        unlink($a->PATH_FILE);
+                    }
+                }
+                $getFilediLampiran = $ttf_lampiran->getDataTtfLampiranByTTfID($data);
+                foreach($getFilediLampiran as $a){
+                    if(file_exists( $a->PATH_FILE )){
+                        unlink($a->PATH_FILE);
+                    }
+                }
+                $deleteLampiran = $ttf_lampiran->deleteTtfLampiran($data);
+                $deleteFpFisik = $sys_fp_fisik->deleteSysFpFisik($nomor_ttf[0]->TTF_NUM);
+                if(file_exists( substr($path_file->PATH_FILE,0,62) )){
+                    rmdir(substr($path_file->PATH_FILE,0,62));
+                }
             }
         },5);
 

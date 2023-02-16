32a1e81a78f92fe27f3b9d16c64ae6e27e1ca722
Vinna Violetta Cornela <Vinna.VC@indomaret.group>
2023-02-16 12:40:30 +0700

    test

--- app/Http/Controllers/testController2.php
+++ app/Http/Controllers/testController2.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\testModel2;
+use App\Models\SysUser;
 
 class testController2 extends Controller
 {
--- app/Http/Controllers/testController3.php
+++ app/Http/Controllers/testController3.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\testModel3;
+use App\Models\testModel4;
 
 class testController3 extends Controller
 {

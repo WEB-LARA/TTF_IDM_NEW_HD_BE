5de8ad2cfb182ff1ec046a9b7ed830f37a3f8121
SiomayBoy <danieladrianss50@gmail.com>
2023-02-16 23:28:40 +0700

    Test

--- app/Http/Controllers/TtfHeaderController.php
+++ app/Http/Controllers/TtfHeaderController.php
@@ -192,16 +192,16 @@ class TtfHeaderController extends Controller
                     }
                 }
             
-                // $deleteLines= $ttf_lines->deleteTtfLines($data);
-                // $deleteFp = $ttf_fp->getFpByTtfId($data);
-                // $deleteHeader = $ttf_header->deleteTtf($data);
+                $deleteLines= $ttf_lines->deleteTtfLines($data);
+                $deleteFp = $ttf_fp->getFpByTtfId($data);
+                $deleteHeader = $ttf_header->deleteTtf($data);
                 $nomor_ttf = $ttf_header->getTtfNumByTtfId($data);
                 $path_file = $ttf_lampiran->getPathFile($data);
                 $deleteLampiran = $ttf_lampiran->deleteTtfLampiran($data);
-                print_r($nomor_ttf);
+                // print_r($nomor_ttf);
                 // print_r("NOMOR_TTF".$nomor_ttf);
-                // $deleteFpFisik = $sys_fp_fisik->deleteSysFpFisik($nomor_ttf[0]->TTF_NUM);
-                // unlink(substr($path_file,0,62));
+                $deleteFpFisik = $sys_fp_fisik->deleteSysFpFisik($nomor_ttf[0]->TTF_NUM);
+                unlink(substr($path_file->PATH_FILE,0,62));
             }
         },5);
 

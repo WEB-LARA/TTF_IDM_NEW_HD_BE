84571eae12b9d979a4c7c59a0b2171bae3a01944
Vinna Violetta Cornela <Vinna.VC@indomaret.group>
2023-02-16 13:23:46 +0700

    test

--- app/Models/testModel3.php
+++ app/Models/testModel3.php
@@ -16,7 +16,7 @@ class testModel3 extends Model
     public $timestamps = false;
 
     public function getDataBranch(){
-            $data = testModel3::select(\DB::raw("CONCAT(sys_ref_branch.BRANCH_CODE,'-',sys_ref_branch.BRANCH_NAME) AS BRANCH"))
+            $data = testModel3::select(\DB::raw("CONCAT(sys_ref_branch.BRANCH_CODE,'-',sys_ref_branch.BRANCH_NAME) AS BRANCH"),'sys_ref_branch.BRANCH_CODE')
             ->orderBy('sys_ref_branch.BRANCH_CODE','ASC')
             ->get();
             return $data;

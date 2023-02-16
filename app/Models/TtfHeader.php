6ee1f24c5fec9c1de4c990082c641262f966ddf9
SiomayBoy <danieladrianss50@gmail.com>
2023-02-16 23:23:12 +0700

    Test

--- app/Models/TtfHeader.php
+++ app/Models/TtfHeader.php
@@ -289,8 +289,9 @@ class TtfHeader extends Model
     }
 
     public function getTtfNumByTtfId($ttf_id){
-        $data = TtfHeader::where('TTF_ID',$ttf_id)->get();
+        // $data = TtfHeader::where('TTF_ID',$ttf_id)->get();
 
+        $data = DB::select('SELECT * FROM TTF_HEADER WHERE TTF_ID = ?',[$ttf_id]);
         print_r($data);
         // return $data;
     }

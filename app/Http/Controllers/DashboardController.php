ea8422f8db81f16a846b226f4da140070761f3d2
SiomayBoy <danieladrianss50@gmail.com>
2023-02-17 17:15:17 +0700

    Test

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,12 +9,12 @@ class DashboardController extends Controller
     //
     public function getDataForInquiryTtfDashboard(Request $request){
         $sys_user = new SysUser();
-
         $data = $sys_user->getDataForInquiryTtfDashboard($request->id_user,$request->branch_code);
-        return response()->json([
-                'status' => 'success',
-                'data' => $data
-        ]);
+        print_r($data);
+        // return response()->json([
+        //         'status' => 'success',
+        //         'data' => $data
+        // ]);
     }
     
 }

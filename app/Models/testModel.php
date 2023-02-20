d9cac492dcf2dc7252ae281bb8c087a73646f798
SiomayBoy <danieladrianss50@gmail.com>
2023-02-20 13:03:24 +0700

    Test

--- app/Models/testModel.php
+++ app/Models/testModel.php
@@ -141,7 +141,7 @@ class testModel extends Model
         if($nofp){
             $data = $data->where('ttf_fp.FP_NUM',$nofp);
         }
-        $data = $data->take(5000)->get();
+        $data = $data->take(10000)->get();
         // print_r($data);        // $data = DB::select("SELECT
         //         ttf_data_bpb.VENDOR_SITE_CODE,
         //         (SELECT 

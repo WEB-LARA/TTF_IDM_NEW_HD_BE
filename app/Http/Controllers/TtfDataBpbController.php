00bc721638dbb9790cc3d10ce27625f272d6e5fb
SiomayBoy <danieladrianss50@gmail.com>
2023-02-24 16:52:55 +0700

    Test Blob

--- app/Http/Controllers/TtfDataBpbController.php
+++ app/Http/Controllers/TtfDataBpbController.php
@@ -8,17 +8,20 @@ use App\Models\TtfDataBpb;
 class TtfDataBpbController extends Controller
 {
     public function getDataBPBPerSupplier(Request $request){
-        $ttf_data_bpb = new TtfDataBpb();
-        $id_bpb = array();
-        if($request->data_bpb){
-            foreach($request->data_bpb as $a){
-                array_push($id_bpb,$a['bpb_id']);
-            }
-        }
-        $getData = $ttf_data_bpb->getDataBPBPerSupplier($request->supp_site_code,$request->branch_code,$id_bpb,$request->session_id,$request->flag_go,$request->flag_ppn,$request->tipe_faktur,$request->jenis_faktur);
-        return response()->json([
-                'status' => 'success',
-                'data' => $getData,
-            ]);
+        print_r("OFFSET = ".$request->offset);
+        echo "<br>";
+        print_r("LIMIT = ".$request->limit);
+        // $ttf_data_bpb = new TtfDataBpb();
+        // $id_bpb = array();
+        // if($request->data_bpb){
+        //     foreach($request->data_bpb as $a){
+        //         array_push($id_bpb,$a['bpb_id']);
+        //     }
+        // }
+        // $getData = $ttf_data_bpb->getDataBPBPerSupplier($request->supp_site_code,$request->branch_code,$id_bpb,$request->session_id,$request->flag_go,$request->flag_ppn,$request->tipe_faktur,$request->jenis_faktur,$request->offset,$request->limit);
+        // return response()->json([
+        //         'status' => 'success',
+        //         'data' => $getData,
+        //     ]);
     }
 }
--- app/Models/TtfDataBpb.php
+++ app/Models/TtfDataBpb.php
@@ -29,7 +29,7 @@ class TtfDataBpb extends Model
         }
     }
 
-    public function getDataBPBPerSupplier($supp_site_code,$branch_code,$notIn,$sess_id,$flag_go,$flag_ppn,$tipe_faktur,$jenis_faktur){
+    public function getDataBPBPerSupplier($supp_site_code,$branch_code,$notIn,$sess_id,$flag_go,$flag_ppn,$tipe_faktur,$jenis_faktur,$offset,$limit){
         if($tipe_faktur == 1){
             if($jenis_faktur == '010'){
                 $data = TtfDataBpb::where('VENDOR_SITE_CODE',$supp_site_code)->where('BRANCH_CODE',$branch_code)->where('USED_FLAG','N')->where('FLAG_GO',$flag_go)->where('FLAG_PPN',$flag_ppn)->whereNotin('BPB_ID',$notIn)->whereRaw('BPB_NUMBER NOT IN (SELECT 

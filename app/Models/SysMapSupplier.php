8d6b46eadb0f5f702d9f9aa83fd3d388a10b16f8
SiomayBoy <danieladrianss50@gmail.com>
2023-02-27 18:41:35 +0700

    Test Blob

--- app/Models/SysMapSupplier.php
+++ app/Models/SysMapSupplier.php
@@ -31,7 +31,7 @@ class SysMapSupplier extends Model
     }
 
     public function getBranchByUserId($user_id){
-        $data = SysMapSupplier::where('USER_ID',$user_id)->select('BRANCH_CODE')->groupBy('BRANCH_CODE')->get();
+        $data = SysMapSupplier::where('USER_IDD',$user_id)->select('BRANCH_CODE')->groupBy('BRANCH_CODE')->get();
 
         return $data;
     }

6ce24a6f7f258e0340ec5b354df119564db3f5df
SiomayBoy <danieladrianss50@gmail.com>
2023-02-27 14:48:23 +0700

    Test Blob

--- app/Http/Controllers/PrepopulatedFpController.php
+++ app/Http/Controllers/PrepopulatedFpController.php
@@ -10,11 +10,12 @@ class PrepopulatedFpController extends Controller
     public function getPrepopulatedFpByNpwp(Request $request){
         $prepopulated_fp = new PrepopulatedFp();
 
-        $data = $prepopulated_fp->getPrepopulatedFpByNpwp($request->npwp,$request->supp_site_code,$request->branch_code,$request->session_id);
+        $data = $prepopulated_fp->getPrepopulatedFpByNpwp($request->npwp,$request->supp_site_code,$request->branch_code,$request->session_id,$request->offset,$request->limit);
 
         return response()->json([
                 'status' => 'success',
-                'data' => $data,
+                'count'=> $data['count'],
+                'data' => $data['data'],
         ]);
     }
 }
--- app/Models/PrepopulatedFp.php
+++ app/Models/PrepopulatedFp.php
@@ -28,14 +28,45 @@ class PrepopulatedFp extends Model
         }
     }
 
-    public function getPrepopulatedFpByNpwp($npwp,$supp_site_code,$branch_code,$session_id){
+    public function getPrepopulatedFpByNpwp($npwp,$supp_site_code,$branch_code,$session_id,$offset,$limit){
+        $skip = ($limit*$offset) - $limit;
         $data = PrepopulatedFp::where('NPWP_PENJUAL',$npwp)->where('USED_FLAG','N')->whereRaw('NOMOR_FAKTUR NOT IN (SELECT 
                                            NO_FP
                                        FROM
                                            ttf_tmp_table
                                        WHERE
                                            SUPP_SITE = ? AND CABANG = ?
-                                               AND SESS_ID = ?)',[$supp_site_code,$branch_code,$session_id])->get();
+                                               AND SESS_ID = ?)',[$supp_site_code,$branch_code,$session_id]);
+        $data_count = $data->count();
+        $data = $data->skip($skip)->take($limit)->get();
+        $nomor = $skip+1;
+        $dataArray = array();
+        $i=0;
+        foreach ($data as $a){
+            // print_r($a->FP_TYPE);
+            // $dataFp = $ttf_fp->getFpByTtfId($request->ttf_id);
+            $dataArray[$i]['NO'] = $nomor;
+            $dataArray[$i]['ID'] = $a->ID;
+            $dataArray[$i]['FG_PENGGANTI'] = $a->FG_PENGGANTI;
+            $dataArray[$i]['JUMLAH_DPP'] = $a->JUMLAH_DPP;
+            $dataArray[$i]['JUMLAH_PPN'] = $a->JUMLAH_PPN;
+            $dataArray[$i]['JUMLAH_PPN_BM'] = $a->JUMLAH_PPN_BM;
+            $dataArray[$i]['KD_JENIS_TRANSAKSI'] = $a->KD_JENIS_TRANSAKSI;
+            $dataArray[$i]['MASA_PAJAK'] = $a->MASA_PAJAK;
+            $dataArray[$i]['NAMA_PEMBELI'] = $a->NAMA_PEMBELI;
+            $dataArray[$i]['NAMA_PENJUAL'] = $a->NAMA_PENJUAL;
+            $dataArray[$i]['NOMOR_FAKTUR'] = $a->NOMOR_FAKTUR;
+            $dataArray[$i]['NPWP_PEMBELI'] = $a->NPWP_PEMBELI;
+            $dataArray[$i]['NPWP_PENJUAL'] = $a->NPWP_PENJUAL;
+            $dataArray[$i]['TAHUN_PAJAK'] = $a->TAHUN_PAJAK;
+            $dataArray[$i]['TANGGAL_FAKTUR'] = $a->TANGGAL_FAKTUR;
+            $dataArray[$i]['EXISTED'] = $a->EXISTED;
+            $dataArray[$i]['USED_FLAG'] = $a->USED_FLAG;
+            $i++;
+            $nomor++;
+        }
+        $return_data['count']=$data_count;
+        $return_data['data']=$dataArray;
 
         return $data;
     }

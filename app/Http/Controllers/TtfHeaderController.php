717a79af05505adcb78f133febe92fd99cf86406
SiomayBoy <danieladrianss50@gmail.com>
2023-03-02 16:31:38 +0700

    TEST1234

--- app/Http/Controllers/TtfHeaderController.php
+++ app/Http/Controllers/TtfHeaderController.php
@@ -268,19 +268,20 @@ class TtfHeaderController extends Controller
                 $path_file = $ttf_header->getPathDirByTtfId($request->TTF_ID);
                 $fileName = $file->hashName();
                 $real_name = $file->getClientOriginalName();
-                if($file->move($path_file['PATH_NOTTF'], $fileName)){
-                    // Convert Fp ke Gambar
-                        return response()->json([
-                            'status' => 'success',
-                            'message' => 'sukses menerima file',
-                            'file_name' => $fileName
-                        ]);
-                }else{
-                        return response()->json([
-                            'status' => 'success',
-                            'message' => 'gagal menerima file',
-                        ]);
-                }
+                print_r($path_file);
+                // if($file->move($path_file['PATH_NOTTF'], $fileName)){
+                //     // Convert Fp ke Gambar
+                //         return response()->json([
+                //             'status' => 'success',
+                //             'message' => 'sukses menerima file',
+                //             'file_name' => $fileName
+                //         ]);
+                // }else{
+                //         return response()->json([
+                //             'status' => 'success',
+                //             'message' => 'gagal menerima file',
+                //         ]);
+                // }
 
             }
         }

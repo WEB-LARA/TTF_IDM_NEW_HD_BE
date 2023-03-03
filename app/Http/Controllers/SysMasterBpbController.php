bc972b96b3953026d1e554d3f7e8aeb34d2723d5
SiomayBoy <danieladrianss50@gmail.com>
2023-03-03 15:39:37 +0700

    test

--- app/Http/Controllers/SysMasterBpbController.php
+++ app/Http/Controllers/SysMasterBpbController.php
@@ -12,7 +12,7 @@ class SysMasterBpbController extends Controller
         $offset=$request->offset;
         $limit=$request->limit;
         $skip = ($limit*$offset) - $limit;
-        $getData = SysMasterBpb::join('sys_ref_branch', 'sys_ref_branch.BRANCH_UNIT_CODE', '=', 'sys_master_bpb.KODE_DC')->select('NO_BPB','TGL_BPB','DPP','TAX','VENDOR_SITE_CODE','INVOICE_NUM','KODE_DC');
+        $getData = SysMasterBpb::join('sys_ref_branch', 'sys_ref_branch.BRANCH_UNIT_CODE', '=', 'sys_master_bpb.KODE_DC')->select('NO_BPBS','TGL_BPB','DPP','TAX','VENDOR_SITE_CODE','INVOICE_NUM','KODE_DC');
         
         $count_data = $getData->count();
 

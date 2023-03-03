995a78912125955bd260ba04b8f1ed3adc989b6f
SiomayBoy <danieladrianss50@gmail.com>
2023-03-03 10:10:57 +0700

    Upload Data blob

--- app/Http/Controllers/TtfHeaderController.php
+++ app/Http/Controllers/TtfHeaderController.php
@@ -262,10 +262,19 @@ class TtfHeaderController extends Controller
 
     public function checkUploadDataBlob(Request $request){
         $ttf_header = new TtfHeader();
-
+        $array_length = count($request->ttf_id);
         print_r($request->ttf_id);
 
         print_r($request->file);
+        for($i = 0 ; $i<$array_length ;$i++){
+            $ttf_id = print_r($request->ttf_id[$i]);
+            $file = print_r($request->file[$i]);
+
+            print_r("TTF ID KE = ".$ttf_id);
+            echo "<br>";
+            print_r("FILE KE = ".$file);
+            echo "<br>";
+        }
         // foreach($request->data as $a){
         //     print_r($a);
         //     $explode_data = explode(",",$a);

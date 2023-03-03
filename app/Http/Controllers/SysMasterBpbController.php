3e46a62f347553c058e1dd5b1dbdc2fc57aad21f
SiomayBoy <danieladrianss50@gmail.com>
2023-03-03 15:38:21 +0700

    test

--- app/Http/Controllers/SysMasterBpbController.php
+++ app/Http/Controllers/SysMasterBpbController.php
@@ -8,7 +8,16 @@ use App\Models\SysMasterBpb;
 class SysMasterBpbController extends Controller
 {
     public function getDataInquiryDownloadBpb(Request $request){
-        $getData = SysMasterBpb::join('sys_ref_branch', 'sys_ref_branch.BRANCH_UNIT_CODE', '=', 'sys_master_bpb.KODE_DC')->select('NO_BPB','TGL_BPB','DPP','TAX','VENDOR_SITE_CODE','INVOICE_NUM','KODE_DC')->get();
+
+        $offset=$request->offset;
+        $limit=$request->limit;
+        $skip = ($limit*$offset) - $limit;
+        $getData = SysMasterBpb::join('sys_ref_branch', 'sys_ref_branch.BRANCH_UNIT_CODE', '=', 'sys_master_bpb.KODE_DC')->select('NO_BPB','TGL_BPB','DPP','TAX','VENDOR_SITE_CODE','INVOICE_NUM','KODE_DC');
+        
+        $count_data = $getData->count();
+
+        $data = $getData->skip($skip)->take($limit)->get();
+    
 
         return response()->json([
                 'status' => 'success',

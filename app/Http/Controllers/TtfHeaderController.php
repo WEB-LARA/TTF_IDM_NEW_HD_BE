fa520d659f23e8dfec6266b10ae2352af3f1f349
SiomayBoy <danieladrianss50@gmail.com>
2023-03-03 09:30:43 +0700

    test1234

--- app/Http/Controllers/TtfHeaderController.php
+++ app/Http/Controllers/TtfHeaderController.php
@@ -262,7 +262,11 @@ class TtfHeaderController extends Controller
 
     public function checkUploadDataBlob(Request $request){
         print_r($request->data);
-        // $ttf_header = new TtfHeader();
+        $ttf_header = new TtfHeader();
+
+        foreach($request->data as $a){
+            print_r($a);
+        }
         // if($request->hasfile('file')){
         //     foreach($request->file as $key => $file)
         //     {

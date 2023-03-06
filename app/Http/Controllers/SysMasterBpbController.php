12c094227819510d001e61cde51b8f93710c96cc
SiomayBoy <danieladrianss50@gmail.com>
2023-03-06 14:40:03 +0700

    test1234

--- app/Http/Controllers/SysMasterBpbController.php
+++ app/Http/Controllers/SysMasterBpbController.php
@@ -68,10 +68,11 @@ class SysMasterBpbController extends Controller
         $getData = SysMasterBpb::where('ID',[17360277])
         ->select('PATH_DATA')->get();
         $pdf = PDFMerger::init();
-        foreach($getData as $a){
-            $pdf->addPDF($a->PATH_DATA, 'all');
-        }
-
+        // foreach($getData as $a){
+        //     $pdf->addPDF($a->PATH_DATA, 'all');
+        // }
+        $pdf->addPDF('/usr/src/app/public/download_bpb/ezlmllnqnjAugIPr2Ee6XBFSOpH06CnSCDNEuFTu.pdf', 'all');
+        $pdf->addPDF('/usr/src/app/public/download_bpb/VBS4So7vcV6ZnwP8LuW2LCJOhlRAvsH6feHPqpNT.pdf', 'all');
         $fileName = time().'.pdf';
         $pdf->merge();
         $pdf->save(public_path('folder_merge_bpb/'.$fileName));

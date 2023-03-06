cdd1cbf26c1b1a5e290e8359aff82c6597ce18e4
SiomayBoy <danieladrianss50@gmail.com>
2023-03-06 16:13:06 +0700

    test1234

--- app/Http/Controllers/SysMasterNrbController.php
+++ app/Http/Controllers/SysMasterNrbController.php
@@ -65,7 +65,7 @@ class SysMasterNrbController extends Controller
     }
 
     public function downloadNrb(Request $request){
-        $getData = SysMasterNrb::whereIn('ID',$request->ID)
+        $getData = SysMasterNrb::whereIn('IDD',$request->ID)
         ->select('PATH_DATA')->get();
         $pdf = PDFMerger::init();
         foreach($getData as $a){
